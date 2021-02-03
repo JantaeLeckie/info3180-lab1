a70e05aeec14e19da940f0f27c53aff2c82ad850
JantaeLeckie <jaleckiie22@gmail.com>
2021-02-03 02:36:39 -0500

    Created my first Flask app

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+#Jantae Leckie
 from flask import Flask, render_template
 
 app = Flask(__name__)
